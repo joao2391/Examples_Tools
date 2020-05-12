92a39324a88363e077cd5e2d821834821cc4c101
joaopaulo2391 <j_paulo_15@hotmail.com>
2020-05-12 05:50:30 -0300

    Jmespath 1st example

---
+++ src/Jmespath/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using DevLab.JmesPath;
+
+
+namespace Jmespath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync("https://jsonplaceholder.typicode.com/todos/1")
+                        .Result.Content.ReadAsStringAsync().Result;
+            
+            const string expression = "@.title";
+
+            var jmes = new JmesPath();            
+            var resultJson = jmes.Transform(result, expression);
+
+            Console.WriteLine(resultJson);
+            Console.ReadKey();
+        }
+    }
+}
